e2deb5d80c4a535a0b6753d2d7a5cb58f7a00cf9
Shreya <shreyalal2025@gmail.com>
2023-03-30 19:03:43 +0530

    Add files via upload

---
+++ assignment-9.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    a = 0
+    b = 1
+    print(a, end=" ")
+    print(b, end=" ")
+    for i in range (n-2):
+        c = a+b
+        print(c, end=" ")
+        a = b
+        b = c
+fibonacci(5)
